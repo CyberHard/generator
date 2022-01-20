14c7ce17b1f8c1c6ad5ff9a571568fa05058ccd1
CyberHard <97868448+CyberHard@users.noreply.github.com>
2022-01-20 11:33:15 +0500

    Add files via upload

---
+++ source/generator.py
@@ -0,0 +1,11 @@
+import random, string
+
+amount = int(input('Укажите количество кодов для генерации: '))
+value = 1
+while value <= amount:
+    code = "https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=16))
+    f = open('Codes.txt', "a+")
+    f.write(f'{code}\n')
+    f.close()
+    print(f'[GENERATED] {code}')
+    value += 1
